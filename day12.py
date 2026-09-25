5e31259e6c8736e4338e97cedda260a570377623
usmannafees001-dev <315257443+usmannafees001-dev@users.noreply.github.com>
2026-09-25 15:39:37 -0700

    ADDEDE day12 -type () and simple CALCULATER

---
+++ day12.py
@@ -0,0 +1,17 @@
+name= "usman nafees"
+profession="developer"
+gpa=3.7
+age=22
+print("my name is,", name, "i am ", age,"years old and my cgpa is,", gpa)
+print(type(name), type(profession), type(gpa))
+# here is the little project on the basis data types(str,int,float),variables,comments
+# simple calculater is here
+a=4
+b=2
+print("a=4,b=2")
+print("addition" ,a+b)
+print("subtraction" ,a-b)
+print("division" ,a/b)
+print("multiplication" ,a*b)
+print("modulus" ,a%b)
+print("floor division" ,a//b)
